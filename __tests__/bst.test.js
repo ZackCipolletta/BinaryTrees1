d854095eb18904575bda417f9b4a549a9d32efa6
Zachary <ZackCipolletta@gmail.com>
2023-08-04 16:02:49 -0700

    write first tests, add gitignore

---
+++ __tests__/bst.test.js
@@ -0,0 +1,15 @@
+import BST from '../src/bst.js';
+
+describe('binarySearchTree', () => {
+
+  test('should initialize a binary search tree with a root of null', () => {
+    let bst = new BST();
+    expect(bst.root).toEqual(null);
+  });
+
+
+  test('it should create a new root node')
+
+
+
+});
