ba14782ae688100f423a828bf2862a2a045fe146
Zachary <ZackCipolletta@gmail.com>
2023-08-07 11:49:23 -0700

    create a test for searching a BST

--- __tests__/bst.test.js
+++ __tests__/bst.test.js
@@ -3,6 +3,7 @@ import BSTNode from '../src/bst.node.js';
 
 describe('binarySearchTree', () => {
 
+
   test('should initialize a binary search tree with a root of null', () => {
     let bst = new BST();
     expect(bst.root).toEqual(null);
@@ -75,5 +76,21 @@ describe('binarySearchTree', () => {
     expect(bst.insertNode(node2)).toEqual({ "root": { "data": 36, "left": null, "right": null } });
   });
 
+
+  const pbst = new BST();
+
+  beforeEach(() => {
+    pbst.insertNode(new BSTNode(4));
+    pbst.insertNode(new BSTNode(2));
+    pbst.insertNode(new BSTNode(6));
+    pbst.insertNode(new BSTNode(1));
+    pbst.insertNode(new BSTNode(3));
+    pbst.insertNode(new BSTNode(5));
+    pbst.insertNode(new BSTNode(7));
+  });
+
+  test('it should return true if the root node is equal to 4', () => {
+    expect(pbst.search(4)).toEqual(true);
+  });
 
 });
